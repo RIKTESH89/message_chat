2ef757879fc87fd9e0e7891f07dfc44e40b602aa
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-14 16:21:03 +0530

    changed blue to gray color in startchat

--- app/startChat/page.jsx
+++ app/startChat/page.jsx
@@ -277,7 +277,7 @@ function StartChatContent() {
                             <div className="flex items-center space-x-2 rtl:space-x-reverse">
                                 <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{createTime(createdAt)}</span>
                             </div>
-                            <div className={`flex flex-col leading-1.5 p-4 border-gray-200 rounded-e-xl rounded-es-xl ${sender !== senderId ? "bg-gray-100 dark:bg-gray-700" : "bg-blue-100 dark:bg-blue-700"}`}>
+                            <div className={`flex flex-col leading-1.5 p-4 border-gray-200 rounded-e-xl rounded-es-xl ${sender !== senderId ? "bg-gray-100 dark:bg-gray-700" : "bg-gray-100 dark:bg-gray-700"}`}>
                                 <p className="text-sm font-normal text-gray-900 dark:text-white">{body}</p>
                             </div>
                         </div>
