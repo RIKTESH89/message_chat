e5c3811136b51d02a7a64512d730fcc727fca005
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-15 03:39:54 +0530

    making password like dots

--- components/InputComponentLikeFirstName.tsx
+++ components/InputComponentLikeFirstName.tsx
@@ -10,12 +10,19 @@ export function InputComponentLikeFirstName({ name, placeholder, onChange }: Inp
     return (
         <div className="m-5 py-3">
             <div className="text-base sm:text-xl font-medium text-left p-2">{name}</div>
+            {name=="Password" ? 
             <input
                 onChange={onChange}
                 className="p-4 border-black shadow w-full"
                 type="password"
                 placeholder={placeholder}
-            />
+            />: 
+            <input
+                onChange={onChange}
+                className="p-4 border-black shadow w-full"
+                type="text"
+                placeholder={placeholder}
+            />}
         </div>
     );
 }
