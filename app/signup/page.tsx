bb541b8c9a26c2b7fb9d1be65446842ffa800fc2
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-15 03:15:55 +0530

    added zod validation on signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -15,6 +15,7 @@ export default function Signup(){
  const [ username, setUserName ] = useState("");
  const [ email, setemail ] = useState("");
  const [ password,setPassword ] = useState("");
+ const [ errorWarning ,seterrorWarning ] = useState(false);
  const router = useRouter();
 
     return (
@@ -23,6 +24,9 @@ export default function Signup(){
 
             <div className="bg-white h-screen flex justify-around">
             <div className="flex flex-col justify-around">
+            {errorWarning ? <div className="p-4 mb-4 text-sm text-red-800 text-center rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                <span className="font-medium">Wrong Inputs!</span> Fill in correct details to sign up.
+            </div>: null}
                 <div className="bg-gray-100 rounded-lg text-center mx-4 mb-16">
                     <Heading>Sign up</Heading>
                     <SubHeading>Enter your Credentials to access your account</SubHeading>
@@ -30,9 +34,15 @@ export default function Signup(){
                     <InputComponentLikeFirstName onChange={function(e){setemail(e.target.value)}} name={"Email"} placeholder={"random@gmail.com"}></InputComponentLikeFirstName>
                     <InputComponentLikeFirstName onChange={function(e){setPassword(e.target.value)}} name={"Password"} placeholder={"123456789"}></InputComponentLikeFirstName>
                     <SignButton onClick={async function(){
-                        const response = await signup(username, password,email);
-                        // alert("Signup Succesful")
+                        try{
+                            const response = await signup(username, password,email);
                         router.push("/signin")
+                        }catch(err){
+                            seterrorWarning(true);
+                            setPassword("");
+                            setUserName("");
+                            setemail("");
+                        }
                     }}>Sign up</SignButton>
                     <BottomWarning Warning={"Already have an account?"} page={"signin"}></BottomWarning>
                 </div>
